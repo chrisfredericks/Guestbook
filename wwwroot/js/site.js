be20f91df0b9ad1e491014f9f7ad6001efaec2cd
chrisfredericks <45493878+chrisfredericks@users.noreply.github.com>
2019-10-22 22:38:20 -0300

    Entry Form Started

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,20 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+$('#addEntryCheck').change(function(){
+    if($(this).prop("checked")) {
+      $('#entryForm').show();
+    } else {
+      $('#entryForm').hide();
+    }
+  });
+
+$('#txtEntry').characterCounter();
+
+//Get the button:
+mybutton = document.getElementById("topButton");
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
