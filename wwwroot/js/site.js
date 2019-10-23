360f5bc9db619dcf1982f87da0fecced0a75f823
chrisfredericks <chrisfredericks08@gmail.com>
2019-10-23 16:21:37 -0300

    Plugin Working

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,15 +1,27 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-$('#addEntryCheck').change(function(){
-    if($(this).prop("checked")) {
-      $('#entryForm').show();
-    } else {
-      $('#entryForm').hide();
-    }
-  });
 
-$('#txtEntry').characterCounter();
+
+  $(document).ready(function () {
+    $('#addEntryCheck').change(function(){
+      if($(this).prop("checked")) {
+        $('#entryForm').show();
+      } else {
+        $('#entryForm').hide();
+      }
+    });
+    
+    $("#txtEntry").charcount({
+      errFontColor:'red',
+      okFontColor:'green',
+      FontWeight:'bold',
+      NumOfCharOfAlert: 20,
+      isAlwaysOn:true
+    });
+});
+  
+  
 
 //Get the button:
 mybutton = document.getElementById("topButton");
